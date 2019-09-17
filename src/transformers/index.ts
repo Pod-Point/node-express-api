753c7334d03f90b508e42e2164e7a8d08f554c77
Lee Brooks <leebrooks93@gmail.com>
2019-09-17 16:51:07 +0100

    Tidy

--- src/transformers/index.ts
+++ src/transformers/index.ts
@@ -1,14 +1,8 @@
-import CardTransformer from './Card';
 import Created from './partials/Created';
 import Deleted from './partials/Deleted';
 import Updated from './partials/Updated';
-import RoleTransformer from './Role';
-import TagTransformer from './Tag';
 import UserTransformer from './User';
 
-export const cardTransformer = new CardTransformer();
-export const roleTransformer = new RoleTransformer();
-export const tagTransformer = new TagTransformer();
 export const userTransformer = new UserTransformer();
 
 export const createdTransformer = new Created();
--- src/types/resources/User.ts
+++ src/types/resources/User.ts
@@ -1,19 +1,11 @@
-import Company from './partials/Company';
 import Created from './partials/Created';
 import Deleted from './partials/Deleted';
 import Updated from './partials/Updated';
-import UserCompany from './partials/UserCompany';
-import UserRole from './partials/UserRole';
-import Role from './Role';
 
 interface User extends Created, Updated, Deleted {
     uid: string;
     name: string;
     email: string;
-    company?: Company;
-    companies?: UserCompany[];
-    role?: Role;
-    roles?: UserRole[];
 }
 
 export default User;
